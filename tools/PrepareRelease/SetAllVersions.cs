56ba1cbe4ef9cc750f6315fff04d778e4bf77e4c
Zach Montoya <zach.montoya@datadoghq.com>
2020-10-09 15:59:36 -0700

    Revert "Use the PrepareRelease tool to update TracerCosntants.AssemblyVersion so the Reflection issues accessing the current AssemblyName are mitigated."
    This reverts commit fbaf26ae6e20ca51529c72b8cd920684b1afadce.

--- tools/PrepareRelease/SetAllVersions.cs
+++ tools/PrepareRelease/SetAllVersions.cs
@@ -67,11 +67,6 @@ namespace PrepareRelease
                 "src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h",
                 FullAssemblyNameReplace);
 
-            // Four-part AssemblyVersion update
-            SynchronizeVersion(
-                "src/Datadog.Trace/TracerConstants.cs",
-                FourPartVersionReplace);
-
             // Locked AssemblyVersion #.0.0.0 updates
             SynchronizeVersion(
                 "src/Datadog.Trace.AspNet/AssemblyInfo.cs",
@@ -113,11 +108,6 @@ namespace PrepareRelease
                 WixProjReplace);
 
             Console.WriteLine($"Completed synchronizing versions to {VersionString()}");
-        }
-
-        private static string FourPartVersionReplace(string text)
-        {
-            return Regex.Replace(text, VersionPattern(fourPartVersion: true), FourPartVersionString(), RegexOptions.Singleline);
         }
 
         private static string FullVersionReplace(string text, string split)
@@ -178,11 +168,6 @@ namespace PrepareRelease
             var newFileContent = transform(fileContent);
 
             File.WriteAllText(fullPath, newFileContent, new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
-        }
-
-        private static string FourPartVersionString(string split = ".")
-        {
-            return $"{TracerVersion.Major}{split}{TracerVersion.Minor}{split}{TracerVersion.Patch}{split}0";
         }
 
         private static string MajorVersionString(string split = ".")
